dec3299a3b6ce695ab1655d22457c0b3bc8f8566
Gideon-Muriithi <gideongakenge@gmail.com>
2019-09-21 22:10:08 +0300

    This commit adds views to to use for testing appearance of the app

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,22 @@
 from flask import render_template
 from . import main
 
+posts = [
+    {
+        'author': 'Gideon M.',
+        'title': 'Blog Post',
+        'content': 'First Post',
+        'date_posted': 'July 1, 2019',
+    },
+
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 1',
+        'content': 'Second Post',
+        'date_posted': 'June  21, 2019',
+    }
+]
+
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
