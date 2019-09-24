f7ffcd148e547444d1dfbcf88e70720ab0705842
Gideon-Muriithi <gideongakenge@gmail.com>
2019-09-24 12:15:15 +0300

    changed config to production

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
         
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://gideon:gidivovo@localhost/blogging_web'
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://gideon:gidivovo@localhost/blogging_web'
 
 
     DEBUG = True
